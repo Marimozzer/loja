82bd403c926a748421b55a64dd3d5bbb4dbaac30
Mariana Mozzer <mari-mozzer@hotmail.com>
2018-05-23 19:10:25 -0300

    Initial commit

---
+++ src/test/java/br/unibh/loja/entidades/Teste.java
@@ -0,0 +1,19 @@
+package br.unibh.loja.entidades;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import org.junit.Test;
+
+public class Teste {
+
+	@Test
+
+	public void testCreateObject() {
+	//Cidade c = new Cliente(1, "Marcos", "marcosmar", "123", "adm", "12345321", "31455556666",
+				//"marcosmar@hotmail.com", 1990-04-12, 2018-05-20);
+		//assertEquals(c.getId(), new Long(1));
+		//assertEquals(c.getNome(), "Marcos");
+
+	}
+
+}
